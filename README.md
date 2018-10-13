7a63778e854b1720fa58a8e231533225bf7ab536
jtomori <tomori.juraj@gmail.com>
2018-10-13 14:38:08 +0200

    writing readme, started doing util scripts for houdini, added cornell.ass scene

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ass_render_time_prediction
+# ASS render time prediction
 Predicting render times for Arnold renderer scenes
+
+<br>
+
+### Versions used
+* Houdini 16.5.571
+* Arnold 5.2.0.1
+* htoa 3.1.1
---
+++ python/houdini_render_time_utils.py
@@ -0,0 +1,40 @@
+"""
+Collection of scripts which are called by Houdini renders.
+"""
+
+import hou
+import time
+
+def timer_start(attrib_name="timer_start"):
+    """
+    Saves current time into hou.session object, this function should be in Pre-Render script
+
+    attrib_name - name of attribute to save to
+    """
+    
+    setattr(hou.session, attrib_name, time.time())
+
+def timer_duration(attrib_name="timer_start"):
+    """
+    Computes and returns difference between current time and time saved in hou.session object
+
+    If attribute doesn't exist, None will be returned
+
+    attrib_name - name of attribute to read from
+    """
+
+    try:
+        return time.time() - getattr(hou.session, attrib_name)
+    except AttributeError:
+        return None
+
+def save_render_time(file=None):
+    """
+    Saves render timwe into a file, this function should be in Post-Render script
+    """
+
+    render_time = timer_duration()
+    if render_time:
+        print "Render took: {} seconds".format(render_time)
+    else:
+        print "None was returned"
