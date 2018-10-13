5f3dd6241452b2779635526ce9132d3fb4560a83
Seyed Ahmad Hosseini <shosseini@medianet.animationsinstitut.de>
2018-10-13 20:42:22 +0200

    cpu info switch to getsys64

--- python/houdini_render_time_utils.py
+++ python/houdini_render_time_utils.py
@@ -8,24 +8,66 @@ import json
 import time
 import cpuinfo
 import subprocess
-#out  =  subprocess.getoutput('wmic MEMORYCHIP get BankLabel,DeviceLocator,Capacity,Tag, MemoryType')
-#print (out)
+import re
+
+re_map = {'Machine': 'Machine: ',
+            'CPU':'CPU  : ',
+            'Cache': 'Cache: ',
+            'Number of cores':'Number of cores: ',
+            'Number of threads': 'Number of threads: '}
+
+def _client_cpu_info(payload):
+    """
+    Get cpu information and relevant features
+    payload(dict): render client information
+    return(dict): payload incl. cpu info
+    """
+    if not cpuinfo.cpu.info:
+        payload['processor_name'] = 'failed'
+        return payload
+
+    if 'ProcessorNameString' in cpuinfo.cpu.info[0]:
+        _processor = cpuinfo.cpu.info[0]['ProcessorNameString']
+        payload['processor_name'], indicators  = _processor, _processor.split()
+        payload['logical_processors'] = len(cpuinfo.cpu.info)
+        payload['GHz'] = float(indicators[-1].replace('GHz', ''))
+        payload['model'] = indicators[1].replace('(R)', '')
+        payload['rev'] = indicators[3]
+        payload['v'] = float(indicators[4].replace('v', ''))
+        payload['abs_GHz'] = payload['logical_processors'] * payload['GHz']
+
+    return payload
+
 
 def client_cpu_info(payload):
     """
-    return cpu info on windows
+    Get cpu information and relevant features using GetSys64.exe
+    payload(dict): render client information
+    return(dict): payload incl. cpu info
     """
-    if cpuinfo.cpu.info:
-        if 'ProcessorNameString' in cpuinfo.cpu.info[0]:
-            _processor = cpuinfo.cpu.info[0]['ProcessorNameString']
-            payload['processor_name'], indicators  = _processor, _processor.split()
-            print(indicators)
-            payload['GHz'] = indicators[-1].replace('GHz', '')
-            payload['model'] = indicators[1].replace('(R)', '')
-            payload['rev'] = indicators[3]
-            payload['v'] = indicators[4].replace('v', '')
-        payload['logical_processors'] = len(cpuinfo.cpu.info)
-    return payload
+    system_info_output = subprocess.getoutput('C://Users//shosseini//Desktop//github//ass_render_time_prediction//utils//GetSys64.exe')
+    for feat, pat in re_map.items():
+        pattern = r"{0}.*".format(pat)
+        ret = re.findall(pattern, system_info_output)[0]
+        if 'CPU' == feat:
+            ret = ret.replace(pat, '')
+            process_indicators = ret.split()
+            payload['sockets'] = float(process_indicators[0])
+            payload['MHz'] = float(process_indicators[-2])
+            payload['id'] = str(' '.join(process_indicators[3:5]))
+            payload['v'] = float(process_indicators[5].replace('v', ''))
+        elif 'Cache' in ret:
+            ret = ret.replace(pat, '')
+            cache_indicators = ret.split()
+            payload['cache KB'] = float(cache_indicators[0])+\
+                               float(cache_indicators[4])+\
+                               float(cache_indicators[8])
+        elif 'Number of cores' == feat:
+            cores_indicators = ret.replace(pat, '')
+            payload['cores'] = float(cores_indicators)
+        elif 'Number of threads' == feat:
+            threads_indicators = ret.replace(pat, '')
+            payload['threads'] = float(threads_indicators)
 
 def timer_start(attrib_name="timer_start"):
     """
@@ -67,5 +109,9 @@ def save_render_time(file=None, payload = {}):
     """
     timer_duration(payload)
     client_cpu_info(payload)
+    print (payload)
     with open('{}.json'.format(file), 'w') as outfile:
         json.dump(payload, outfile)
+
+
+save_render_time()
