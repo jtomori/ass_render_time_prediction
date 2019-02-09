bda5e0b48cf33d36ff578119c9784ac2fb35b76a
jtomori <tomori.juraj@gmail.com>
2019-02-09 20:15:04 +0100

    referenced parameters instead of copy-pasting them

--- scripts/python/ass_prediction.py
+++ scripts/python/ass_prediction.py
@@ -108,30 +108,7 @@ class HoudiniArnoldSamples(GenericSamples):
         """
         Returns a dictionary with parameter values over frame range. This can be used to save animated values of parameters which are not found in logs.
         """
-        parameters = [
-            "ar_AA_samples",
-            "ar_GI_diffuse_samples",
-            "ar_GI_specular_samples",
-            "ar_GI_transmission_samples",
-            "ar_GI_sss_samples",
-            "ar_GI_volume_samples",
-            "ar_indirect_specular_blur",
-            "ar_sss_use_autobump",
-            "ar_GI_total_depth",
-            "ar_GI_diffuse_depth",
-            "ar_GI_specular_depth",
-            "ar_GI_transmission_depth",
-            "ar_GI_volume_depth",
-            "ar_auto_transparency_depth",
-            "ar_low_light_threshold",
-            "ar_mb_xform_keys",
-            "ar_mb_dform_keys",
-            "ar_bucket_size",
-            "ar_pixel_filter_width",
-            "ar_indirect_sample_clamp",
-            "res_overridex",
-            "res_overridey"
-        ]
+        parameters = cls.parameters.keys()
 
         out_dict = {}
 
